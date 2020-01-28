f19e1c7840810f5a4a49040a86d38188a3d8e219
Alfredo <alfredo.pena@zipdev.co>
2020-01-28 10:40:19 -0600

    initial commit

---
+++ .babelrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                "modules": "commonjs",
+                "debug": false
+            }
+        ],
+        /// your presets
+    ],
+    plugins: [
+        //... your plugins
+    ]
+};
